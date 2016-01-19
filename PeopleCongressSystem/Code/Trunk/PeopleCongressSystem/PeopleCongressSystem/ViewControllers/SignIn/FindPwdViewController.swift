a21b78ddb7bc874e9c57a2f3d467bf1a8a226df2
hokagequan <hokagequan@gmail.com>
2016-01-19 22:27:32 +0800

    找回密码

---
+++ PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/ViewControllers/SignIn/FindPwdViewController.swift
@@ -0,0 +1,55 @@
+//
+//  FindPwdViewController.swift
+//  PeopleCongressSystem
+//
+//  Created by Matt Quan on 16/1/19.
+//  Copyright © 2016年 CoolRabbit. All rights reserved.
+//
+
+import UIKit
+
+class FindPwdViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var mobileTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func clickSubmit(sender: AnyObject) {
+        // TODO: 找回密码
+    }
+    
+    @IBAction func hideKeyboard(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - UITextField
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
