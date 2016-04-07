ae0663c8d61862ddef5dda4f9aad99a4b8127d1d
hokagequan <hokagequan@gmail.com>
2016-04-07 11:23:50 +0800

    推送注册

--- PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/HttpRequests/SubmitDeviceTokenReq.swift
+++ PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/HttpRequests/SubmitDeviceTokenReq.swift
@@ -19,11 +19,13 @@ class SubmitDeviceTokenReq: HttpBaseReq {
     func requestSimpleCompletion() {
         var params = [String: String]()
         params["phoneID"] = deviceToken
-        params["UserId"] = PCSDataManager.defaultManager().accountManager.user?.congressID
+        params["UserId"] = PCSDataManager.defaultManager().accountManager.user?.identifier
         params["FieldID"] = PCSDataManager.defaultManager().accountManager.user!.field
+        params["CheckTicket"] = PCSDataManager.defaultManager().accountManager.user!.token
         params["thecharset"] = "gb2312"
         
         self.request("setappENS", nameSpace: "gonggong", params: params) { (response) -> Void in
+            print("")
         }
     }
     
