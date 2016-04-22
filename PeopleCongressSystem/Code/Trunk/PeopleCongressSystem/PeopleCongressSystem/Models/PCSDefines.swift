ad57f0531b65507173b5859afaa483ade5b2e767
hokagequan <hokagequan@gmail.com>
2016-04-22 10:19:24 +0800

    服务器

--- PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/Models/PCSDefines.swift
+++ PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/Models/PCSDefines.swift
@@ -8,18 +8,19 @@
 
 import Foundation
 
-/// @brief Server
-let serverURL1 = "http://218.61.34.40:8099/"
-let imageDownloadURL = "http://218.61.34.40:8099/lvzhiImage/"
-let serverSoapAction = "http://tempuri.org/"
-let photoDownloadURL = "http://218.61.34.40:8099/File/headimage/"
-let qrCodeDownloadURL = "http://218.61.34.40:8099/File/QRCode/"
-
-//let serverURL1 = "http://dlrenda.dlpii.com:8099/"
-//let imageDownloadURL = "http://175.170.128.160:8099/lvzhiImage/"
+/// @brief Server 测试
+//let serverURL1 = "http://218.61.34.40:8099/"
+//let imageDownloadURL = "http://218.61.34.40:8099/lvzhiImage/"
 //let serverSoapAction = "http://tempuri.org/"
-//let photoDownloadURL = "http://dlrenda.dlpii.com:8099/File/headimage/"
-//let qrCodeDownloadURL = "http://dlrenda.dlpii.com:8099/File/QRCode/"
+//let photoDownloadURL = "http://218.61.34.40:8099/File/headimage/"
+//let qrCodeDownloadURL = "http://218.61.34.40:8099/File/QRCode/"
+
+/// @brief Server 正式
+let serverURL1 = "http://dlrenda.dlpii.com:8099/"
+let imageDownloadURL = "http://dlrenda.dlpii.com:8099/lvzhiImage/"
+let serverSoapAction = "http://tempuri.org/"
+let photoDownloadURL = "http://dlrenda.dlpii.com:8099/File/headimage/"
+let qrCodeDownloadURL = "http://dlrenda.dlpii.com:8099/File/QRCode/"
 
 /// @brief HTML Page
 let pageHTMLVariableManagerWorker = "apph5/iphone/GZRY_lvzhiguanli.aspx?"
