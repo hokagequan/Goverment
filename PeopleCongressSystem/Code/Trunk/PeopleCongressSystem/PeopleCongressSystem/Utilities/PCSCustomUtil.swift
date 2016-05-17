2ebd88e0719f7ba6a74ce4f45e9e34912da7db99
hokagequan <Q@MattdeMacBook-Pro.local>
2016-05-17 10:59:03 +0800

    【IOS】把App里面出现“大连市人大代表履职系统”字样的地方，都去掉“大连市”3个字，（只有关于里面的大连市3个字不用去掉）

--- PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/Utilities/PCSCustomUtil.swift
+++ PeopleCongressSystem/Code/Trunk/PeopleCongressSystem/PeopleCongressSystem/Utilities/PCSCustomUtil.swift
@@ -23,7 +23,7 @@ class PCSCustomUtil: NSObject {
         imageView.frame = CGRectMake(0, 0, image!.size.width * 0.8, image!.size.height * 0.8)
         let leftItem = UIBarButtonItem(customView: imageView)
         vc.navigationItem.leftBarButtonItem = leftItem
-        vc.navigationItem.title = "大连市人大代表履职信息管理系统"
+        vc.navigationItem.title = "人大代表履职信息管理系统"
         vc.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.systemFontOfSize(15.0)]
     }
     
